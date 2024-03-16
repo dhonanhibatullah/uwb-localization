9cc3d20a849e35b86dc1094bc42f9c0426a240d1
dhonanhibatullah <dhonan.hibatullah@gmail.com>
2024-03-16 12:10:18 +0700

    Fix!: Major fix, fully ROS2-style dev

--- app/firmware/esp32-uwb-localization/include/dw1k_conn.h
+++
@@ -1,6 +0,0 @@
-#ifndef __DW1K_CONN_H__
-#define __DW1K_CONN_H__
-
-
-
-#endif
---
+++ app/src/uwbloc_interfaces/app/src/app.js
@@ -0,0 +1,18 @@
+const path      = require('path')
+const express   = require('express')
+const rclga     = require('./js/rcl-gateaway')
+
+const app       = express()
+const port      = 3000
+
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '../views'))
+app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
+
+
+
+app.get('/', (req, req) => {
+
+})
